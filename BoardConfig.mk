a87cb8f9f343218fb31e0b5d8f88ad13c78272f1
Martinusbe <martinusbe@gmail.com>
2017-03-17 20:44:03 -0500

    angler: Lockscreen charging info: real time values
    Change-Id: I5f1775a110daedd605439da9df1fe16106d5e913

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -167,4 +167,7 @@ BOARD_HARDWARE_CLASS := \
     hardware/cyanogen/cmhw \
     device/huawei/angler/cmhw
 
+# Enable real time lockscreen charging current values
+BOARD_GLOBAL_CFLAGS += -DBATTERY_REAL_INFO
+
 -include vendor/huawei/angler/BoardConfigVendor.mk
