11cc866f53d95a7a6a4eff50a293ba4354e3ebae
Dustin Rinne <dustin.rinne@gmail.com>
2017-06-04 14:58:14 -0500

    angler: update build fingerprint & description to N2G47O

--- validus.mk
+++ validus.mk
@@ -29,15 +29,15 @@ DEVICE_PACKAGE_OVERLAYS += device/huawei/angler/overlay-cm
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := validus_angler
 PRODUCT_DEVICE := angler
-PRODUCT_BRAND := google
+PRODUCT_BRAND := Google
 PRODUCT_MODEL := Nexus 6P
 PRODUCT_MANUFACTURER := Huawei
 
 # Device Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=angler \
-    BUILD_FINGERPRINT=google/angler/angler:7.1.2/N2G47H/3783593:user/release-keys \
-    PRIVATE_BUILD_DESC="angler-user 7.1.2 N2G47H 3783593 release-keys"
+    BUILD_FINGERPRINT=google/angler/angler:7.1.2/N2G47O/3852959:user/release-keys \
+    PRIVATE_BUILD_DESC="angler-user 7.1.2 N2G47O 3852959 release-keys"
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2560
